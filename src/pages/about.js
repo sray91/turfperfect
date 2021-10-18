821ba1956e823b4b3511d828605cc5b9633e287a
sr447011 <rayswanagan@gmail.com>
2021-10-17 23:45:26 -0400

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+//import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About">
+        <p>We strive to be the best turf integrator in the Lowcountry!</p>
+      </Layout>
+    )
+}
+
+export default AboutPage
---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+//import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+const ContactPage = () => {
+    return (
+        <Layout pageTitle="Contact">
+        <p>Please reach out for more information!</p>
+        <p>Call, text, or email.</p>
+      </Layout>
+    )
+}
+
+export default ContactPage
